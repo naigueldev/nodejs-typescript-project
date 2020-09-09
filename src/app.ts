06d069e3cb0ada1a283eb3d7987fb07e99c61838
Naiguel Santos <naiguelsantosdeveloper@gmail.com>
2020-09-09 01:21:11 -0300

    prettier fix

--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,23 @@ import { application } from 'express';
 import bodyParser from 'body-parser';
 import * as http from 'http';
 
-
 export class SetupApplication {
-    private server?: http.Server;
+  private server?: http.Server;
 
-    constructor(private port = 3000, public app = application) { }
+  constructor(private port = 3000, public app = application) {}
 
-    public init(): void {
-        this.setupExpress();
-    }
+  public init(): void {
+    this.setupExpress();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
 
-    public start(): void {
-        this.server = this.app.listen("3000", () => {
-            console.log('Server running on port 3000')
-        });
-    }
+  public start(): void {
+    this.server = this.app.listen('3000', () => {
+      console.log('Server running on port 3000');
+    });
+  }
 }
