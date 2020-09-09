44aae1754e8829aca78cc5b0095065a4beff5e0b
Naiguel Santos <naiguelsantosdeveloper@gmail.com>
2020-09-09 01:33:54 -0300

    Add hello world route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import './util/module-alias';
-import { application } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import * as http from 'http';
 
 export class SetupApplication {
   private server?: http.Server;
 
-  constructor(private port = 3000, public app = application) {}
+  constructor(private port = 3000, public app = express()) { }
 
   public init(): void {
     this.setupExpress();
+    this.setupRoutes();
+  }
+
+  private setupRoutes(): void {
+    this.app.get('/', (request, response) => {
+      response.send('Hello world!');
+    });
   }
 
   private setupExpress(): void {
@@ -18,8 +25,8 @@ export class SetupApplication {
   }
 
   public start(): void {
-    this.server = this.app.listen('3000', () => {
-      console.log('Server running on port 3000');
+    this.server = this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
     });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { SetupApplication } from './app';
 
 const application = new SetupApplication(3333);
+application.init();
 application.start();
